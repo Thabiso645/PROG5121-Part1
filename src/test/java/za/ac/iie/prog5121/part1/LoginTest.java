eb0956f3433277892b951fea5fd6d1d16e0e57ba
Thabiso645 <st10514400@rcconnect.edu.za>
2026-09-18 12:10:07 +0200

    Add JUnit tests for Login class
    Added JUnit tests to verify login functionality.

--- src/test/java/za/ac/iie/prog5121/part1/LoginTest.java
+++ src/test/java/za/ac/iie/prog5121/part1/LoginTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+Added JUnit tests for the login class
 /**
  * JUnit tests for the login class
  */
